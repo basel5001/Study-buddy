643fc7f07f40ac8c5608a2e0e0ac55b0b16846b5
Basel Mohamed Alam Elden <35863315+basel5001@users.noreply.github.com>
2026-01-19 17:46:08 +0200

    Delete encryptApiUrl.js

--- encryptApiUrl.js
+++
@@ -1,9 +0,0 @@
-// Run this script ONCE to get your encrypted API URL
-// Usage: node encryptApiUrl.js
-const CryptoJS = require('crypto-js');
-
-const apiUrl = 'https://7pz7euzg1d.execute-api.us-west-2.amazonaws.com/Dev'; // Your API URL
-const key = 'mySecretKey123'; // Change this to your secret key
-
-const encrypted = CryptoJS.AES.encrypt(apiUrl, key).toString();
-console.log('Encrypted API URL:', encrypted);
